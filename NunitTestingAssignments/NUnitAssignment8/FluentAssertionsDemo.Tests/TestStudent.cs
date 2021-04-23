c999f4286b67d3b7557f449d42d9b316c0ee8740
Parth Joshi <parth.joshi@thegatewaycorp.com>
2021-04-23 14:50:37 +0530

    Assinment8 Added

---
+++ NunitTestingAssignments/NUnitAssignment8/FluentAssertionsDemo.Tests/TestStudent.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using static FluentAssertionsDemo.STD;
+
+namespace FluentAssertionsDemo.Tests
+{
+    public class TestStudent
+    {
+        [OrderedTest(1)]
+        public void Test_GetAllStudent()
+        {
+            // Act
+            Student result = STD.GetStudentById(2);
+            // Assert
+            Assert.That(result, Has.Property("Name").EqualTo("Parth"));
+        }
+        [OrderedTest(3)]
+        public void Test_TotalStudent()
+        {
+            // Assert
+            Assert.That(STD.GetAllStudent(), Has.Count.EqualTo(3).And.All.Property("Name").Not.Null.And.All.Property("Percentage").GreaterThan(95));
+        }
+        [OrderedTest(2)]
+        public void Test_GetStudentNameById()
+        {
+            // Assert
+            Assert.That(STD.GetStudentNameById(1), Is.EqualTo("Parth"));
+        }
+        [OrderedTest(0)]
+        public void Test_GetFirstRank()
+        {
+            // Arrange
+            Student result = new Student() { Id = 1, Name = "Parth", Branch = "IT", Percentage = 99 };
+
+            // Assert
+            Assert.That(STD.GetFirstRank(), Has.Property("Name").EqualTo(result.Name).And.Property("Branch").EqualTo(result.Branch));
+        }
+
+        [TestCaseSource(sourceName: "TestSource")]
+        public void MainTest(TestStructure test)
+        {
+            test.Test();
+        }
+
+        public static IEnumerable<TestCaseData> TestSource
+        {
+            get
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                Dictionary<int, List<MethodInfo>> methods = assembly
+                .GetTypes()
+                .SelectMany(x => x.GetMethods())
+                .Where(y => y.GetCustomAttributes().OfType<OrderedTestAttribute>().Any())
+                .GroupBy(z => z.GetCustomAttribute<OrderedTestAttribute>().Ord)
+                .ToDictionary(gdc => gdc.Key, gdc => gdc.ToList());
+                foreach (var order in methods.Keys.OrderBy(x => x))
+                {
+                    foreach (var methodInfo in methods[order])
+                    {
+                        MethodInfo info = methodInfo;
+                        yield return new TestCaseData(
+                        new TestStructure
+                        {
+                            Test = () =>
+                            {
+                                object classInstance =
+                                Activator.CreateInstance(info.DeclaringType, null);
+                                info.Invoke(classInstance, null);
+                            }
+                        }).SetName(methodInfo.Name);
+                    }
+                }
+            }
+        }
+    }
+    public class TestStructure
+    {
+        public Action Test;
+    }
+
+    public class OrderedTestAttribute : Attribute
+        {
+            public int Ord { get; set; }
+            public OrderedTestAttribute(int ord)
+            {
+                this.Ord = ord;
+            }
+        }
+    }
+
---
+++ NunitTestingAssignments/NUnitAssignment8/FluentAssertionsDemo/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentAssertionsDemo
+{
+    public static class STD
+    {
+        static List<Student> student = new List<Student>()
+        {
+            new Student(){Id=1,Name="Parth",Branch="IT",Percentage=99},
+            new Student(){Id=2,Name="Parth",Branch="CE",Percentage=98},
+            new Student(){Id=3,Name="Parth",Branch="EE",Percentage=97},
+        };
+        public static List<Student> GetAllStudent()
+        {
+            return student;
+        }
+        public static List<Student> GetStudentsByBranch(string branch)
+        {
+            List<Student> std = student.Where(x => x.Branch == branch).ToList();
+            if(std!=null)
+            {
+                throw new Exception("Not Found");
+            }
+            return std;
+        }
+
+        public static Student GetStudentById(int id)
+        {
+            Student std = student.Where(x => x.Id == id).FirstOrDefault();
+            if (std == null)
+            {
+                throw new Exception("Not Found");
+            }
+            return std;
+        }
+        public static string GetStudentNameById(int id)
+        {
+            Student std = student.Where(x => x.Id == id).FirstOrDefault();
+            return std.Name;
+        }
+        public static int GetTotalStudent()
+        {
+            int c = 0;
+            c = GetAllStudent().Count();
+            return c;
+        }
+        public static Student GetFirstRank()
+        {
+            Student std = new Student();
+            std = GetAllStudent().OrderByDescending(x => x.Percentage).FirstOrDefault();
+            return std;
+
+        }
+        public class Student
+        {
+            public int Id { get; set; }
+            public String Name { get; set; }
+            public String Branch { get; set; }
+            public decimal Percentage { get; set; }
+        }
+    }
+}
